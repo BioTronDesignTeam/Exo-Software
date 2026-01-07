db9e514160202eddda9dc0f53ed411c77dd35c41
Adityya Kaushal <adityyakaushal@gmail.com>
2026-01-07 18:09:55 -0500

    init: Added docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,42 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://biotrondesignteam.github.io',
+  base: '/Exo-Software/',
+  integrations: [
+    starlight({
+      customCss: [
+        './src/styles/custom.css',
+      ],
+      expressiveCode: {
+        themes: ['github-light-high-contrast', 'tokyo-night'],
+        removeUnusedThemes: true,
+        styleOverrides: {
+          // You can also override styles
+          borderRadius: '0.5rem',
+          borderColor: ['gray', 'black'],
+          frames: {
+            terminalTitlebarBackground: ['#414868', 'lightGray'],
+            terminalTitlebarBorderBottomColor: ['gray', 'black'],
+            terminalTitlebarDotsOpacity: '0.5',
+            terminalTitlebarForeground: ['white', 'black']
+          }
+        },
+      },
+      components: {
+        ThemeSelect: './src/components/ThemeSelect.astro',
+        Pagination: './src/components/Pagination.astro',
+      },
+      title: 'BioTron',
+      sidebar: [
+        {
+          label: 'Start Here',
+          autogenerate: { directory: 'start-here' },
+        },
+      ],
+    }),
+  ],
+});
